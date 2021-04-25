bacfa20cb8dc1b446fa9846c4903820f22c95b16
Suryadip Bandyopadhyay <sband2000@gmail.com>
2021-04-25 13:18:41 -0500

    added image textures

--- main.cc
+++ main.cc
@@ -25,6 +25,13 @@ hittable_list two_perlin_spheres() {
     return objects;
 }
 
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+    return hittable_list(globe);
+}
+
 hittable_list random_scene() {
     hittable_list world;
 
@@ -122,9 +129,15 @@ int main() {
             lookat = point3(0, 0, 0);
             vfov = 20.0;
             break;
-        default:
         case 3:
             world = two_perlin_spheres();
+            lookfrom = point3(13, 2, 3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+            break;
+        default:
+        case 4:
+            world = earth();
             lookfrom = point3(13, 2, 3);
             lookat = point3(0, 0, 0);
             vfov = 20.0;
--- texture.h
+++ texture.h
@@ -2,7 +2,10 @@
 #define TEXTURE_H
 
 #include "rtweekend.h"
+#include "rtw_stb_image.h"
 #include "perlin.h"
+
+#include <iostream>
 
 class texture {
     public:
@@ -61,5 +64,52 @@ class noise_texture : public texture {
         double scale;
 };
 
+
+class image_texture : public texture {
+    public:
+        const static int bytes_per_pixel = 3;
+
+        image_texture() : data(nullptr), width(0), height(0), bytes_per_scanline(0) {}
+
+        image_texture(const char* filename) {
+            auto components_per_pixel = bytes_per_pixel;
+            data = stbi_load(filename, &width, &height, &components_per_pixel, components_per_pixel);
+            if (!data) {
+                std::cerr << "ERROR: Could not load texture image file '" << filename << "'.\n";
+                width = height = 0;
+            }
+            bytes_per_scanline = bytes_per_pixel * width;
+        }
+
+        ~image_texture() {
+            delete data;
+        }
+
+        virtual color value(double u, double v, const vec3& p) const override {
+            // default to solid cyan
+            if (data == nullptr)
+                return color(0, 1, 1);
+            
+            u = clamp(u, 0.0, 1.0);
+            v = 1.0 - clamp(v, 0.0, 1.0); // flips to image coords
+            auto i = static_cast<int>(u * width);
+            auto j = static_cast<int>(v * height);
+
+            if (i >= width)
+                i = width - 1;
+            if (j >= height)
+                j = height - 1;
+            
+            const auto color_scale = 1.0 / 255.0;
+            auto pixel = data + j * bytes_per_scanline + i * bytes_per_pixel;
+            return color(color_scale * pixel[0], color_scale * pixel[1], color_scale * pixel[2]);
+        }
+
+    private:
+        unsigned char* data;
+        int width;
+        int height;
+        int bytes_per_scanline;
+};
 
 #endif
